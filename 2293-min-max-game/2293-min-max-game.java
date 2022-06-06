62cddb525f423934f5aeab156b7023eb0fc65d47
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-06 18:57:02 +0530

    Time: 2 ms (75.00%), Space: 44.2 MB (25.00%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,25 +1,16 @@
 class Solution {
     public int minMaxGame(int[] nums) {
-        int[]arr=new int[nums.length/2];
-        while(nums.length!=1){
-            
-        for(int i=0;i<nums.length/2;i++)
-        {
-            
-            if(i%2==0)
-            {
-                arr[i]=Math.min(nums[2*i],nums[2*i+1]);
-                
-            }else{
-                arr[i]=Math.max(nums[2*i],nums[2*i+1]);
+        int[] arr = new int[nums.length / 2];
+        while (nums.length != 1) {
+            for (int i = 0; i < nums.length / 2; i++) {
+                if (i % 2 == 0) {
+                    arr[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    arr[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
             }
-            
-            
-               
-
-        }
-        nums=arr;
-       arr=new int[nums.length/2];
+            nums = arr;
+            arr = new int[nums.length / 2];
         }
         return nums[0];
     }
