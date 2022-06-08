f3488c8c11e7d4213c168409c9ff5b19b335925a
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-08 17:32:05 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (18.75%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,22 +1,16 @@
 class Solution {
+
     public int removePalindromeSub(String s) {
-       int c=0;
-        int l=0;
-        int h=s.length()-1;
-        while(l<h)
-        {
-            if(s.charAt(l)==s.charAt(h))
-            {
-                l++;
-                h--;
-                 
-            }else{
-                //c+=2;
-                l++;
-                h--;
-                c+=1;
+        int c = 0;
+        int l = 0;
+        int h = s.length() - 1;
+        while (l < h) {
+            if (s.charAt(l) != s.charAt(h)) {
+                c += 1;
             }
+            l++;
+            h--;
         }
-        return c>0?2:1;
+        return c > 0 ? 2 : 1;
     }
 }
