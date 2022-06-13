e5c1803dbfa0601896ae139c80e4aac2953842d0
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-13 21:57:50 +0530

    Time: 4 ms (69.63%), Space: 58.1 MB (13.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,24 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
-        int count=0;
-        boolean[][]visit=new boolean[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++){
-                
-                if(visit[i][j]==false && grid[i][j]=='1'){
+        int count = 0;
+        boolean[][] visit = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if ( grid[i][j] == '1') {
                     count++;
-                help(grid,i,j,visit);
+                    help(grid, i, j, visit);
                 }
-                
             }
         }
         return count;
     }
 
-    public void help(char[][] grid,int sr,int sc,boolean[][]visit) {
-        if (sr<0 || sc<0 || sr>=grid.length|| sc>=grid[0].length|| grid[sr][sc]=='0'||visit[sr][sc]==true)
-            return ;
-        visit[sr][sc]=true;
-        help(grid, sr + 1, sc,visit);
-        help(grid, sr, sc + 1,visit);
-        help(grid, sr - 1, sc,visit);
-        help(grid, sr , sc - 1,visit);
+    public void help(char[][] grid, int sr, int sc, boolean[][] visit) {
+        if (sr < 0 || sc < 0 || sr >= grid.length || sc >= grid[0].length || grid[sr][sc] == '0') return;
+        grid[sr][sc] = '0';
+        help(grid, sr + 1, sc, visit);
+        help(grid, sr, sc + 1, visit);
+        help(grid, sr - 1, sc, visit);
+        help(grid, sr, sc - 1, visit);
     }
 }
