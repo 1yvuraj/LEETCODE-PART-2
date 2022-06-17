a84548b0948a62f637aeb4ab7dcb2b9d8ed0e977
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-17 14:13:11 +0530

    Time: 1 ms (76.65%), Space: 43.4 MB (52.90%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,32 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-	int count = 0 ;
-	final int CAMERA = 1 ;
-	final int MONITORED = 2 ;
-	final int NOT_MONITORED = 3 ;
-
-	public int cameraCounter(TreeNode root){
-
-		if(root == null)
-			return MONITORED ;
-
-		int leftcall = cameraCounter(root.left) ;
-		int rightcall = cameraCounter(root.right) ;
-
-		if(leftcall == NOT_MONITORED || rightcall == NOT_MONITORED){
-			count++ ;
-			return CAMERA ;
-		}
-
-		else if(leftcall == CAMERA || rightcall == CAMERA)
-			return MONITORED ;
-
-		else
-			return NOT_MONITORED ;
-
-
-	}
-
-	public int minCameraCover(TreeNode root) {
-		return cameraCounter(root) == NOT_MONITORED ? count+1 : count ;
-	}
+      int cameras = 0;
+    
+   public  int MinCamerasInBT_(TreeNode node){
+        if(node == null)return 1;
+        
+        int left = MinCamerasInBT_(node.left);
+        int right = MinCamerasInBT_(node.right);
+        
+        if(left == -1 || right == -1){
+            cameras++;
+            return 0;
+        }
+        
+        if(left == 0 || right == 0){
+            return 1;
+        }
+        return -1;
+    }
+    
+    public int minCameraCover(TreeNode root) {
+        //int cameras = 0;
+        if(MinCamerasInBT_(root) == -1)cameras++;
+        return cameras;
+    }
 }
