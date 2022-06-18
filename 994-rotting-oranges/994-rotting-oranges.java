8e6bf74fb36f174513f3cfe81d408e771dbe97d8
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-18 20:14:18 +0530

    Time: 4 ms (42.71%), Space: 43.7 MB (12.20%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,54 +1,45 @@
 class Solution {
-    public class pair{
-        int x;
-        int y;
-        int t;
-        pair(int x,int y,int t){
-            this.x=x;
-            this.y=y;
-            this.t=t;
-        }
-        
-    }
     public int orangesRotting(int[][] grid) {
-       return  help(grid);
-    }
-    public int help(int[][] grid) {
-        int fresh=0;
-        LinkedList<pair>list=new LinkedList<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1)
-                {
+        int fresh = 0;
+        int time = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
                     fresh++;
                 }
-                if(grid[i][j]==2){
-                    list.addLast(new pair(i,j,0));
+                if (grid[i][j] == 2) {
+                    list.addLast(i * m + j);
                 }
             }
         }
-       int time =0;
-        while(list.size()>0)
-        {
-            pair rem=list.removeFirst();
-            if( rem.x<0 || rem.y<0 || rem.x>=grid.length||rem.y>=grid[0].length || grid[rem.x][rem.y]==-1||grid[rem.x][rem.y]==0)
-            {
-                continue;
+        if(fresh==0)return 0;
+        while (list.size() > 0) {
+            int size = list.size();
+
+            while (size-->0) {
+                int rem = list.removeFirst();
+                int sr = rem / m;
+                int sc = rem % m;
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == 1) {
+                        grid[r][c] = 2;
+                        list.addLast(r * m + c);
+                        fresh--;
+                        if (fresh == 0) {
+                            return time + 1;
+                        }
+                    }
+                }
+                
             }
-            
-            if(rem.t>time)
-            {
-                time=rem.t;
-            }
-            if(grid[rem.x][rem.y]==1){
-                fresh--;
-            }
-            grid[rem.x][rem.y]=-1;
-            list.addLast(new pair(rem.x+1,rem.y,rem.t+1));
-            list.addLast(new pair(rem.x,rem.y+1,rem.t+1));
-            list.addLast(new pair(rem.x-1,rem.y,rem.t+1));
-            list.addLast(new pair(rem.x,rem.y-1,rem.t+1));
+            time++;
         }
-        return fresh==0?time:-1;
+        return -1;
     }
 }
