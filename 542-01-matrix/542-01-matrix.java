030240add36199832c4a4dcbde75c93d28ff22a0
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-18 23:37:11 +0530

    Time: 24 ms (39.30%), Space: 73.8 MB (25.57%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,40 +1,42 @@
 class Solution {
-    public class pair{
-        int level;
-        int i;
-        int j;
-        pair(int level,int i,int j)
+    public int[][] updateMatrix(int[][] grid) {
+        LinkedList<Integer> list = new LinkedList<>();
+        int[][]visit=new int[grid.length][grid[0].length];
+        int m = grid[0].length;
+        int n = grid.length;
+        for(int i=0;i<grid.length;i++)
         {
-            this.level=level;
-            this.i=i;
-            this.j=j;
-        }
-    }
-    public int[][] updateMatrix(int[][] mat) {
-        LinkedList<pair>list=new LinkedList<>();
-        int[][]arr=new int[mat.length][mat[0].length];
-        boolean[][]visit=new boolean[mat.length][mat[0].length];
-        for(int i=0;i<mat.length;i++)
-        {
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==0)
-                list.addLast(new pair(0,i,j));
-            }
-        }
-        while(list.size()>0)
-        {
-            pair rem=list.removeFirst();
-            if(rem.i<0 || rem.j<0 || rem.i>=mat.length || rem.j>=mat[0].length||visit[rem.i][rem.j]==true)
+            for(int j=0;j<grid[0].length;j++)
             {
-                continue;
+                if(grid[i][j]==0)
+                {
+                    list.add(i*m+j);
+                    visit[i][j]=2;
+                }
             }
-            visit[rem.i][rem.j]=true;
-            arr[rem.i][rem.j]=rem.level;
-            list.addLast(new pair(rem.level+1,rem.i-1,rem.j));
-            list.addLast(new pair(rem.level+1,rem.i,rem.j-1));
-            list.addLast(new pair(rem.level+1,rem.i+1,rem.j));
-            list.addLast(new pair(rem.level+1,rem.i,rem.j+1));
         }
-        return arr;
+        
+        int[][] dir = { { -1, 0 },{ 0, 1 },{ 1, 0 }, { 0, -1 }};
+        int time = 1;
+        while (list.size() > 0) {
+            int size = list.size();
+            while (size-->0) {
+                int rem = list.removeFirst();
+                int sr = rem / m;
+                int sc = rem % m;
+                
+                for (int d = 0; d < dir.length; d++) {
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && visit[r][c] != 2) {
+                        grid[r][c] = grid[sr][sc]+1;
+                        visit[r][c]=2;
+                        list.add(r * m + c);
+                    }
+                }
+            }
+            
+        }
+        return grid;
     }
 }
