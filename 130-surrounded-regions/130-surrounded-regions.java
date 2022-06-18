4eba996c17dff7c7b6a2c1a461cbf93ff36fa3ed
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-18 11:08:24 +0530

    Time: 3 ms (52.94%), Space: 51.5 MB (61.07%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,52 +1,46 @@
 class Solution {
     public void solve(char[][] grid) {
-        int row = grid.length,col = grid[0].length;
-		for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++){
-                if(i==0||j==0||i==grid.length-1||j==grid[0].length-1){
-                    if(grid[i][j]=='O'){
-                    help(grid,i,j);
+        int[][]dir={{-1,0},{0,-1},{1,0},{0,1}};
+        int row = grid.length, col = grid[0].length;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) {
+                    if (grid[i][j] == 'O') {
+                        help(grid, i, j,dir);
                     }
                 }
             }
         }
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            { 
-                if(grid[i][j]=='v'){
-                    grid[i][j]='O';
-                 }
-                else {
-                    grid[i][j]='X';
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 'v') {
+                    grid[i][j] = 'O';
+                } else {
+                    grid[i][j] = 'X';
                 }
             }
         }
-        
     }
-    public void help(char[][] grid,int row,int col) {
-        if(row<0 || col<0 || row>=grid.length || col>=grid[0].length||grid[row][col]!='O'){
-            return ;
+
+    public void help(char[][] grid, int row, int col,int[][]dir) {
+        grid[row][col] = 'v';
+        for(int d=0;d<dir.length;d++)
+        {
+            int r=row+dir[d][0];
+            int c=col+dir[d][1];
+            if (r >=0 && c>= 0 && r < grid.length && c< grid[0].length && grid[r][c]== 'O'){
+                help(grid,r,c,dir);
+            }
         }
-        grid[row][col]='v';
-        help(grid,row+1,col);
-        help(grid,row-1,col);
-        help(grid,row,col-1);
-        help(grid,row,col+1);
-        
     }
 }
 // class Solution {
 //     public void solve(char[][] board) {
-        
 //         // Lengths of board
 //         int n = board.length;
 //         int m = board[0].length;
-        
 //         if(n==0 || m==0) return;
-        
 //         // The base cases
 //         for(int i=0;i<n;i++){
 //             for(int j=0;j<m;j++){
@@ -55,7 +49,6 @@ class Solution {
 //                 }
 //             }
 //         }
-        
 //         //Final Iteration
 //         for(int i=0;i<n;i++){
 //             for(int j=0;j<m;j++){
@@ -66,21 +59,15 @@ class Solution {
 //                 }
 //             }
 //         }
-        
 //     }
-    
 //     public static void dfs(int i,int j,char[][] board,int n,int m){
-        
 //         if(i<0 || j<0 || i>n-1 || j>m-1 || board[i][j]!='O'){
 //             return;
 //         }
 //         board[i][j]='#';
-
 //         dfs(i+1,j,board,n,m);
 //         dfs(i-1,j,board,n,m);
 //         dfs(i,j+1,board,n,m);
 //         dfs(i,j-1,board,n,m);
-        
 //     }
-    
 // }
