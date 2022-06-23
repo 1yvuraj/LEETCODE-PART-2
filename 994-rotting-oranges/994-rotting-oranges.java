5f21ef340e0b99b463c8bde08cb3f70ef92363f0
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-23 13:07:30 +0530

    Time: 4 ms (43.68%), Space: 42.6 MB (71.36%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -2,42 +2,48 @@ class Solution {
 
     public int orangesRotting(int[][] grid) {
         int fresh = 0;
-        int time = 1;
+        int time = 0;
         int n = grid.length;
         int m = grid[0].length;
         int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.addLast(i*m+j);
+                }else if(grid[i][j]==1){
                     fresh++;
-                }
-                if (grid[i][j] == 2) {
-                    list.addLast(i * m + j);
                 }
             }
         }
-        if (fresh == 0) return 0;
-        while (list.size() > 0) {
-            int size = list.size();
-
-            while (size-- > 0) {
-                int rem = list.removeFirst();
-                int sr = rem / m;
-                int sc = rem % m;
-
-                for (int d = 0; d < dir.length; d++) {
-                    int r = sr + dir[d][0];
-                    int c = sc + dir[d][1];
-                    if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == 1) {
-                        grid[r][c] = 2;
-                        list.addLast(r * m + c);
+        if(fresh==0)return 0;
+        while(q.size()>0)
+        {
+            int size=q.size();
+            while(size-->0)
+            {
+                int rem=q.removeFirst();
+                int sr=rem/m;
+                int sc=rem%m;
+                
+                for(int d=0;d<dir.length;d++)
+                {
+                    int r=sr+dir[d][0];
+                    int c=sc+dir[d][1];
+                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1){
+                        grid[r][c]=2;
+                        q.addLast(r*m+c);
                         fresh--;
-                        if (fresh == 0) {
-                            return time;
+                        if(fresh==0)
+                        {
+                            return time+1;
                         }
                     }
                 }
+                
             }
             time++;
         }
