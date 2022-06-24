bd5ff7568ac1d3164254d37d2edbc79e2656ae7b
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-24 11:21:37 +0530

    Time: 1 ms (86.32%), Space: 44.1 MB (36.50%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] arr;
+    int[] rank;
+    public int[] findRedundantConnection(int[][] edges) {
+        int[] ans = new int[2];
+        arr = new int[edges.length+1];
+        rank = new int[edges.length+1];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=i;
+        }
+        for (int[] c : edges) {
+            int row = c[0];
+            int col = c[1];
+            int src1 = find(row);
+            int src2 = find(col);
+            if (src1 != src2) {
+                union(src1, src2);
+            } else {
+                ans[0] = row;
+                ans[1] = col;
+                return ans;
+            }
+        }
+        ans=new int[1];
+        ans[0]=-1;
+        return ans;
+    }
+
+    public int find(int x) {
+        if (arr[x] == x) {
+            return x;
+        }
+        int temp = find(arr[x]);
+        arr[x] = temp;
+        return temp;
+    }
+
+    public void union(int x, int y) {
+        if (rank[x] > rank[y]) {
+            arr[y] = x;
+        } else if (rank[x] < rank[y]) {
+            arr[x] = y;
+        } else {
+            arr[x] = y;
+            rank[x]++;
+        }
+    }
+}
