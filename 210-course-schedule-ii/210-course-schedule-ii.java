af17c7a00550a86532e3cf0ebbd8e9029d1a02eb
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-29 12:41:20 +0530

    Time: 2 ms (100.00%), Space: 43.6 MB (86.67%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,50 +1,37 @@
 class Solution {
+    int idx ;
+
     public int[] findOrder(int n, int[][] prerequisites) {
-        ArrayList<Integer>[]graph=new ArrayList[n];
-        for(int i=0;i<n;i++)
-        {
-            graph[i]=new ArrayList<>();
+        idx=n-1;
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        for(int[]arr:prerequisites){
-            graph[arr[0]].add(arr[1]);
+        for (int[] arr : prerequisites) {
+            graph[arr[1]].add(arr[0]);
         }
-        int[]arr=new int[n];
-        LinkedList<Integer>q=new LinkedList<>();
-        for(int i=0;i<n;i++)
-        {
-            for(int nbr:graph[i]){
-                arr[nbr]++;
+        int[] visit = new int[n];
+        int[] arr = new int[n];
+        boolean cycle = false;
+        for (int i = 0; i < graph.length; i++) {
+            if (visit[i] == 0) cycle = cycle || dfs(i, graph, visit, arr);
+        }
+        if(!cycle) return arr;
+        return new int[0];
+    }
+
+    public boolean dfs(int src, ArrayList<Integer>[] graph, int[] visit, int[] arr) {
+        visit[src] = 1;
+        boolean icycle = false;
+        for (int nbr : graph[src]) {
+            if (visit[nbr] == 0) {
+                icycle = icycle || dfs(nbr, graph, visit,arr);
+            } else if (visit[nbr] == 1) {
+                return true;
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==0)
-            {
-                q.addLast(i);
-            }
-        }
-        int idx=arr.length-1;
-        int[]ans=new int[n];
-        while(q.size()>0)
-        {
-            int rem=q.removeFirst();
-            ans[idx]=rem;
-            idx--;
-           
-            for(int nbr:graph[rem]){
-                arr[nbr]--;
-                if(arr[nbr]==0)
-                {
-                    q.addLast(nbr);
-                }
-            }
-        }
-        
-        if(idx==-1){
-            return ans;
-            
-        }
-        return arr=new int[0];
-        
+        visit[src] = 2;
+        arr[idx--] = src;
+        return icycle;
     }
 }
