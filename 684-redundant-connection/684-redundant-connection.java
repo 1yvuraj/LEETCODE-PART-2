e84b4de221ce9cf24b25b3891f1d789793610124
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-06-30 13:12:57 +0530

    Time: 1 ms (87.23%), Space: 43.9 MB (48.96%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -2,47 +2,36 @@ class Solution {
     int[] arr;
     int[] rank;
     public int[] findRedundantConnection(int[][] edges) {
-        int[] ans = new int[2];
         arr = new int[edges.length+1];
         rank = new int[edges.length+1];
-        for(int i=0;i<arr.length;i++)
-        {
+        for(int i=0;i<edges.length;i++){
             arr[i]=i;
+            rank[i]=1;
         }
-        for (int[] c : edges) {
-            int row = c[0];
-            int col = c[1];
-            int src1 = find(row);
-            int src2 = find(col);
-            if (src1 != src2) {
-                union(src1, src2);
-            } else {
-                ans[0] = row;
-                ans[1] = col;
-                return c;
+        for(int[]nbr:edges){
+            int u=nbr[0];
+            int v=nbr[1];
+            int x=find(u);
+            int y=find(v);
+            if(x!=y){
+                union(x,y);
+            }else{
+                return nbr;
             }
         }
-        
         return null;
     }
-
     public int find(int x) {
-        if (arr[x] == x) {
-            return x;
-        }
-        int temp = find(arr[x]);
-        arr[x] = temp;
-        return temp;
+       return arr[x]==x?x:(arr[x]=find(arr[x]));
     }
-
-    public void union(int x, int y) {
-        if (rank[x] > rank[y]) {
-            arr[y] = x;
-        } else if (rank[x] < rank[y]) {
-            arr[x] = y;
-        } else {
-            arr[x] = y;
-            rank[x]++;
-        }
+    public void union(int x,int y) {
+       if(rank[x]>rank[y]){
+           arr[y]=x;
+       }else if(rank[x]>rank[y]){
+           arr[x]=y;
+       }else{
+          arr[x]=y;
+          rank[x]++;
+       }
     }
 }
