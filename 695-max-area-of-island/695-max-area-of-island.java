b30f59aadafd3fbcc120f953329885ed8125d966
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-19 15:23:32 +0530

    Time: 6 ms (25.46%), Space: 49 MB (12.00%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,34 +1,49 @@
 class Solution {
-    int max = -(int) 1e9;
-
     public int maxAreaOfIsland(int[][] grid) {
         int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
-        boolean[][] visit = new boolean[grid.length][grid[0].length];
-
+        int max=-(int)1e9;
+        LinkedList<Integer>q=new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (visit[i][j] == false && grid[i][j] == 1) {
-                    visit[i][j] = true;
-                    
-                    max = Math.max(max,dfs(i, j, visit, dir, grid));
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j]==1){
+
+                    max=Math.max(max,bfs(grid,i,j,dir));
                 }
             }
         }
-        return max == -(int) 1e9 ? 0 : max;
+        return max==-(int)1e9?0:max;
+    
+       
     }
-
-    public int dfs(int sr, int sc, boolean[][] visit, int[][] dir, int[][] grid) {
-        int size = 0;
-        for (int d = 0; d < dir.length; d++) {
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-
-            if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == 1 && visit[r][c] == false) {
-                visit[r][c] = true;
-
-                size += dfs(r, c, visit, dir, grid);
+    public int bfs(int[][] grid,int i,int j,int[][]dir) {
+        LinkedList<Integer>q=new LinkedList<>();
+        q.addLast(i*grid[0].length+j);
+        int l=1;
+        grid[i][j]=0;
+        while(q.size()>0){
+            int size=q.size();
+            
+            while(size-->0){
+                int rem=q.removeFirst();
+                int sr=rem/grid[0].length;
+                int sc=rem%grid[0].length;
+                
+                
+                for(int d=0;d<dir.length;d++){
+                    int r=sr+dir[d][0];
+                    int c=sc+dir[d][1];
+                    if(r>=0 && c>=0 && r<grid.length && c<grid[0].length  && grid[r][c]==1){
+                        l++;
+                        grid[r][c]=0;
+                        q.addLast(r*grid[0].length + c);
+                    }
+                }
             }
+            
+           
         }
-        return size + 1;
+        return l;
     }
+
+    
 }
