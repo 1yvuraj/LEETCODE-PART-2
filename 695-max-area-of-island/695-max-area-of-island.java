ad3ca1fe33cc43aa1d9c309c86bd07da0106a327
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-19 13:18:41 +0530

    Time: 6 ms (25.46%), Space: 46.8 MB (74.42%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,35 +1,34 @@
 class Solution {
-    int max=-(int)1e9;
-    int l=1;
+    int max = -(int) 1e9;
+
     public int maxAreaOfIsland(int[][] grid) {
-        int[][]dir={{-1,0},{0,-1},{1,0},{0,1}};
-        boolean[][]visit=new boolean[grid.length][grid[0].length];
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            
-            for(int j=0;j<grid[i].length;j++){
-                if(visit[i][j]==false && grid[i][j]==1){
-                    l=1;
-                    visit[i][j]=true;
-                    dfs(i,j,visit,dir,grid);
-                max=Math.max(max,l);
+        int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+        boolean[][] visit = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (visit[i][j] == false && grid[i][j] == 1) {
+                    visit[i][j] = true;
+                    
+                    max = Math.max(max,dfs(i, j, visit, dir, grid));
                 }
             }
         }
-        return max==-(int)1e9?0:max;
+        return max == -(int) 1e9 ? 0 : max;
     }
-    public void dfs(int sr,int sc,boolean[][]visit,int[][]dir,int[][]grid) {
-        for(int d=0;d<dir.length;d++)
-        {
-            int r=sr+dir[d][0];
-            int c=sc+dir[d][1];
-            
-            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c]==1 && visit[r][c]==false){
-               visit[r][c]=true;
-                l++;
-               dfs(r,c,visit,dir,grid); 
+
+    public int dfs(int sr, int sc, boolean[][] visit, int[][] dir, int[][] grid) {
+        int size = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == 1 && visit[r][c] == false) {
+                visit[r][c] = true;
+
+                size += dfs(r, c, visit, dir, grid);
             }
         }
+        return size + 1;
     }
 }
