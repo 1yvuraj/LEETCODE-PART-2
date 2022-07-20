a5412da1fe83160264e10536da7e65d2ce6e0c0c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-20 23:11:38 +0530

    Time: 6 ms (41.53%), Space: 50.9 MB (88.99%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,30 +1,54 @@
 class Solution {
+    int max = -(int) 1e9;
+    int[] p;
+    int[] rank;
+    int ones = 0;
+
     public int numIslands(char[][] grid) {
-        int [][]dir={{-1,0},{0,1},{0,-1},{1,0}};
-        int c=0;
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            {
-               
-                if(grid[i][j]=='1'){
-                dfs(grid,i,j,dir);
-                     c++;
+        p = new int[grid.length * grid[0].length];
+        rank = new int[grid.length * grid[0].length];
+        int max = -(int) 1e9;
+        int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+        for (int i = 0; i < p.length; i++) {
+            p[i] = i;
+            rank[i] = 1;
+        }
+        int ans=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]-'0' == 1) {
+                 ans++;
                 }
             }
         }
-        return c;
-    }
-    public void dfs(char[][] grid,int sr,int sc,int[][]dir) {
-        grid[sr][sc]='0';
-        for(int d=0;d<dir.length;d++)
-        {
-            
-            int r=sr+dir[d][0];
-            int c=sc+dir[d][1];
-            if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c]=='1'){
-             dfs(grid,r,c,dir);
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]-'0' == 1) {
+                   
+                    int x = find(i * grid[0].length + j);
+                    for (int d = 0; d < dir.length; d++) {
+                        int r = i + dir[d][0];
+                        int c = j + dir[d][1];
+                        if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c]-'0' == 1) {
+                            int y = find(r * grid[0].length + c);
+                            if (x != y) {
+                                p[y] = x;
+                                ans--;
+                                rank[x] += rank[y];
+                            }
+                        }
+                    }
+                   
+                }
             }
         }
+        return ans;
+    }
+
+    public int find(int x) {
+        return p[x] == x ? x : (p[x] = find(p[x]));
     }
 }
+
+    
