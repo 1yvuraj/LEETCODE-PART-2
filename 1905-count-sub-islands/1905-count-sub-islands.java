b0dbc146f285bdcbb14905e765b6d0f65fe4bf42
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-20 18:53:27 +0530

    Time: 52 ms (17.95%), Space: 141.3 MB (25.37%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+
+        int ans = 0;
+        for (int i = 0; i < grid2.length; i++) {
+            for (int j = 0; j < grid2[0].length; j++) {
+                if (grid2[i][j] == 1) {
+                    if (dfs(i, j, dir, grid1,grid2)) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public boolean dfs(int sr, int sc, int[][] dir, int[][] grid1,int[][] grid2) {
+        boolean res = true;
+        
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < grid2.length && c < grid2[0].length && grid2[r][c] == 1) {
+                grid2[r][c] = 0;
+
+                res = dfs(r, c, dir, grid1,grid2) &&res;
+            }
+        }
+        return res && (grid1[sr][sc] == 1);
+    }
+}
