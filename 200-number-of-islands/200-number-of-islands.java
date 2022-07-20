913de0d4805ffe972eb1890229e2c31b7c15e9e5
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-20 23:25:56 +0530

    Time: 5 ms (55.84%), Space: 50.7 MB (92.34%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,8 +1,6 @@
 class Solution {
-    int max = -(int) 1e9;
     int[] p;
     int[] rank;
-    int ones = 0;
 
     public int numIslands(char[][] grid) {
         p = new int[grid.length * grid[0].length];
@@ -13,24 +11,24 @@ class Solution {
             p[i] = i;
             rank[i] = 1;
         }
-        int ans=0;
+        int ans = 0;
+        // for (int i = 0; i < grid.length; i++) {
+        //     for (int j = 0; j < grid[0].length; j++) {
+        //         if (grid[i][j] - '0' == 1) {
+        //             ans++;
+        //         }
+        //     }
+        // }
+
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j]-'0' == 1) {
-                 ans++;
-                }
-            }
-        }
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j]-'0' == 1) {
-                   
+                if (grid[i][j] - '0' == 1) {
+                     ans++;
                     int x = find(i * grid[0].length + j);
                     for (int d = 0; d < dir.length; d++) {
                         int r = i + dir[d][0];
                         int c = j + dir[d][1];
-                        if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c]-'0' == 1) {
+                        if (r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] - '0' == 1) {
                             int y = find(r * grid[0].length + c);
                             if (x != y) {
                                 p[y] = x;
@@ -39,7 +37,6 @@ class Solution {
                             }
                         }
                     }
-                   
                 }
             }
         }
@@ -50,5 +47,3 @@ class Solution {
         return p[x] == x ? x : (p[x] = find(p[x]));
     }
 }
-
-    
