1e4ad7e8ad30772162ca51684133aaae28ebfc66
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-22 11:28:17 +0530

    Time: 2 ms (47.93%), Space: 48.4 MB (47.11%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,34 +1,31 @@
 class Solution {
-    public int[] sortArrayByParity(int[] inputArr) {
-        int[] arr1 = new int[inputArr.length];
-        int[] arr2 = new int[inputArr.length];
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = -1;
-        }
-        int k=0,j=0;
-        for (int i = 0; i < inputArr.length; i++) {
-            if(inputArr[i]%2==0){
-                arr1[k]=inputArr[i];
-                k++;
+    public int[] sortArrayByParity(int[] nums) {
+         int i = 0;
+        int j = 0;
+        int[] copy = nums.clone();
+        int n = nums.length;
+        
+        while(i < n){
+            if(nums[i] % 2 == 1){
+                i++;
             }else{
-                 arr2[j]=inputArr[i];
+                swap(nums,i,j);
+                i++;
                 j++;
             }
         }
-        k=0;
-        for(int i=0;i<arr1.length;i++){
-             if(arr1[i]!=-1){
-            inputArr[k]=arr1[i];
-            k++;
-             }
-        }
-        
-        for(int i=0;i<arr2.length;i++){
-            if(k<inputArr.length && arr2[i]!=-1){
-            inputArr[k]=arr2[i];
-            k++;
+        i = 0;
+        for(;i<n; i++){
+            if(copy[i] % 2 != 0){
+                nums[j++] = copy[i];
             }
         }
-        return inputArr;
+        return nums;
+    }
+    private static void swap(int[] nums, int i, int j)
+    {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
