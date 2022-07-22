d0f51f73cc0585941d30f28cbe93905b5d2e2a8d
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-22 10:56:22 +0530

    Time: 2 ms (47.93%), Space: 48 MB (73.66%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        int i = 0;
-        int j = nums.length - 1;
-        while (i <= j) {
-            if (nums[i] % 2 == 1 && nums[j] % 2 == 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j--;
-                i++;
-            } 
-            else if(nums[i]%2==1)
-            {
-                j--;
+    public int[] sortArrayByParity(int[] inputArr) {
+        int[] arr1 = new int[inputArr.length];
+        int[] arr2 = new int[inputArr.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = -1;
+        }
+        int k=0,j=0;
+        for (int i = 0; i < inputArr.length; i++) {
+            if(inputArr[i]%2==0){
+                arr1[k]=inputArr[i];
+                k++;
             }else{
-                i++;
+                 arr2[j]=inputArr[i];
+                j++;
             }
         }
-        return nums;
+        k=0;
+        for(int i=0;i<arr1.length;i++){
+             if(arr1[i]!=-1){
+            inputArr[k]=arr1[i];
+            k++;
+             }
+        }
+        
+        for(int i=0;i<arr2.length;i++){
+            if(k<inputArr.length && arr2[i]!=-1){
+            inputArr[k]=arr2[i];
+            k++;
+            }
+        }
+        return inputArr;
     }
 }
