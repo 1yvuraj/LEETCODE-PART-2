b0962a397d2a5a48beae3425625cf62f85dab83a
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-24 11:01:03 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (75.00%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[]arr=new int[26];
+        char ans='a';
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            if( arr[s.charAt(i)-'a']==2){
+                return s.charAt(i);
+            }
+           
+        }
+        
+        return ans;
+       
+    }
+}
