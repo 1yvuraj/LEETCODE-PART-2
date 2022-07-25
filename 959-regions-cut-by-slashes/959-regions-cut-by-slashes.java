89aa55b6ae4817e95513e87542986b0017179724
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-25 23:25:28 +0530

    Time: 2 ms (100.00%), Space: 40.6 MB (93.62%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
@@ -22,54 +22,54 @@ class Solution {
         }
     }
 
-    // public int regionsBySlashes(String[] grid) {
-    //     int n = grid.length;
-    //     int N = (grid.length + 1) * (grid.length + 1);
-    //     p = new int[N];
-    //     r = new int[N];
-    //     int count = 1;
-    //     for (int i = 0; i < p.length; i++) {
-    //         if (i / N == 0 || i / N == N - 1 || i % N == 0 || i % N == N - 1) {
-    //             p[i] = 0;
-    //         } else {
-    //             p[i] = i;
-    //         }
-    //     }
-    //     for (int i = 0; i < grid.length; i++) {
-    //         String s = grid[i];
-    //         for (int j = 0; j < s.length(); j++) {
-    //             if (s.charAt(j) == '/') {
-    //                 count += union(findPar(i * N + j + 1), findPar((i + 1) * N + j));
-    //             } else if (s.charAt(j) == '\\') {
-    //                 count += union(findPar(i * N + j), findPar((i + 1) * N + j + 1));
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
-     public int regionsBySlashes(String[] grid) {
+    public int regionsBySlashes(String[] grid) {
         int n = grid.length;
-        int N = n + 1;
-        p = new int[N * N];
-        r = new int[N * N];
-        for (int i = 0; i < N * N; i++)
-            if (i / N == 0 || i % N == 0 || i / N == N - 1 || i % N == N - 1)
+        int N = n+1;
+        p = new int[N*N];
+        r = new int[N*N];
+        int count = 1;
+        for (int i = 0; i < p.length; i++) {
+            if (i / N == 0 || i / N == N - 1 || i % N == 0 || i % N == N - 1) {
                 p[i] = 0;
-            else
+            } else {
                 p[i] = i;
-
-        int regions = 1;
-        for (int r = 0; r < n; r++) {
-            String s = grid[r];
-            for (int c = 0; c < s.length(); c++) {
-                if (s.charAt(c) == '/')
-                    regions += union(findPar(r * N + c + 1), findPar((r + 1) * N + c));
-                else if (s.charAt(c) == '\\')
-                    regions += union(findPar(r * N + c), findPar((r + 1) * N + c + 1));
             }
         }
-
-        return regions;
+        for (int i = 0; i < grid.length; i++) {
+            String s = grid[i];
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '/') {
+                    count += union(findPar(i * N + j + 1), findPar((i + 1) * N + j));
+                } else if (s.charAt(j) == '\\') {
+                    count += union(findPar(i * N + j), findPar((i + 1) * N + j + 1));
+                }
+            }
+        }
+        return count;
     }
+//      public int regionsBySlashes(String[] grid) {
+//         int n = grid.length;
+//         int N = n + 1;
+//         p = new int[N * N];
+//         r = new int[N * N];
+//         for (int i = 0; i < N * N; i++)
+//             if (i / N == 0 || i % N == 0 || i / N == N - 1 || i % N == N - 1)
+//                 p[i] = 0;
+//             else
+//                 p[i] = i;
+
+//         int regions = 1;
+//         for (int r = 0; r < n; r++) {
+//             String s = grid[r];
+//             for (int c = 0; c < s.length(); c++) {
+//                 if (s.charAt(c) == '/')
+//                     regions += union(findPar(r * N + c + 1), findPar((r + 1) * N + c));
+//                 else if (s.charAt(c) == '\\')
+//                     regions += union(findPar(r * N + c), findPar((r + 1) * N + c + 1));
+//             }
+//         }
+
+//         return regions;
+//     }
 
 }
