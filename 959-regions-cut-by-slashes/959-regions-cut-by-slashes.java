3fff42272acad733c822193eb0fe574d10e75108
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-25 23:24:23 +0530

    Time: 4 ms (88.59%), Space: 42 MB (76.68%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
@@ -1,84 +1,75 @@
 class Solution {
-    int[] arr;
-    int[] rank;
-    int count;
-    public int regionsBySlashes(String[] grid) {
-        count=1;
-        int n=grid.length;
-        arr = new int[(n+1)*(n+1)];
-        rank = new int[(n+1)*(n+1)];
-        for(int i=0;i<arr.length;i++)
-        {
-            arr[i]=i;
-            rank[i]=1;
+    int[] p;
+    int[] r;
+
+    public int findPar(int u) {
+        return p[u] == u ? u : (p[u] = findPar(p[u]));
+    }
+
+    public int union(int x, int y) {
+        if (x != y) {
+            if (r[x] > r[y]) {
+                p[y] = x;
+            } else if (r[x] < r[y]) {
+                p[x] = y;
+            } else {
+                p[x] = y;
+                r[x]++;
+            }
+            return 0;
+        } else {
+            return 1;
         }
-        for(int i=0;i<=grid.length;i++)
-        {
-            for(int j=0;j<=grid.length;j++)
-            {
-                if(i==0 || j==0 ||i==grid.length || j==grid.length)
-                {
-                    int cellno=i*(grid.length+1)+j;
-                    if(cellno!=0){
-                    help(0,cellno);
-                    }
-                }
+    }
+
+    // public int regionsBySlashes(String[] grid) {
+    //     int n = grid.length;
+    //     int N = (grid.length + 1) * (grid.length + 1);
+    //     p = new int[N];
+    //     r = new int[N];
+    //     int count = 1;
+    //     for (int i = 0; i < p.length; i++) {
+    //         if (i / N == 0 || i / N == N - 1 || i % N == 0 || i % N == N - 1) {
+    //             p[i] = 0;
+    //         } else {
+    //             p[i] = i;
+    //         }
+    //     }
+    //     for (int i = 0; i < grid.length; i++) {
+    //         String s = grid[i];
+    //         for (int j = 0; j < s.length(); j++) {
+    //             if (s.charAt(j) == '/') {
+    //                 count += union(findPar(i * N + j + 1), findPar((i + 1) * N + j));
+    //             } else if (s.charAt(j) == '\\') {
+    //                 count += union(findPar(i * N + j), findPar((i + 1) * N + j + 1));
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+     public int regionsBySlashes(String[] grid) {
+        int n = grid.length;
+        int N = n + 1;
+        p = new int[N * N];
+        r = new int[N * N];
+        for (int i = 0; i < N * N; i++)
+            if (i / N == 0 || i % N == 0 || i / N == N - 1 || i % N == N - 1)
+                p[i] = 0;
+            else
+                p[i] = i;
+
+        int regions = 1;
+        for (int r = 0; r < n; r++) {
+            String s = grid[r];
+            for (int c = 0; c < s.length(); c++) {
+                if (s.charAt(c) == '/')
+                    regions += union(findPar(r * N + c + 1), findPar((r + 1) * N + c));
+                else if (s.charAt(c) == '\\')
+                    regions += union(findPar(r * N + c), findPar((r + 1) * N + c + 1));
             }
         }
-        for(int i=0;i<grid.length;i++)
-        {
-            char[]ch=grid[i].toCharArray()
-;            for(int j=0;j<grid.length;j++)
-            {
-                
-                if(ch[j]=='/')
-                {
-                    int cell1=i*(grid.length+1)+j+1;
-                    int cell2=(i+1)*(grid.length+1)+j;
-                    help(cell1,cell2);
-                }else if(ch[j]=='\\'){
-                    int cell1=i*(grid.length+1)+j;
-                    int cell2=(i+1)*(grid.length+1)+j+1;
-                    help(cell1,cell2);
-                }
-            }
-        }
-        return count;
-        
+
+        return regions;
     }
-    public void help(int x,int y)
-    {
-        int x1=find(x);
-        int y1=find(y);
-        if(x1!=y1)
-        {
-            union(x1,y1);
-        }
-        else{
-            count++;
-        }
-    }
-    public int find(int x)
-    {
-        if(arr[x]==x)
-        {
-            return x;
-        }
-        int temp=find(arr[x]);
-        arr[x]=temp;
-        return temp;
-    }
-    public void union(int x,int y)
-    {
-       
-        if(rank[x]>rank[y]){
-            arr[y]=x;
-        }else if(rank[x]<rank[y]){
-            arr[x]=y;
-        }else{
-            arr[x]=y;
-            rank[x]++;
-        }
-        
-    }
+
 }
