cb4cd17a9c11df4161f37d460f357cfb4cc6012f
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-07-31 22:30:33 +0530

    Time: 3 ms (37.50%), Space: 42 MB (37.50%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int minimumOperations(int[] nums) {
+        int[] arr = new int[101];
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) arr[nums[i]]++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
