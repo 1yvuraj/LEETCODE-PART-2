4ade20d30154f7b8238457956179ba3aad9d5d3e
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-02 00:05:21 +0530

    Time: 2417 ms (16.67%), Space: 122.5 MB (33.33%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -15,16 +15,16 @@ class Solution {
         int[] B = new int[n];
         Arrays.fill(A, Integer.MAX_VALUE);
         Arrays.fill(B, Integer.MAX_VALUE);
-        distace(graph, node1, node2, node1,A);
-     distace(graph, node1, node2, node2,B);
+        distace(graph, node1, node2, node1, A);
+        distace(graph, node1, node2, node2, B);
 
         int min = (int) 1e9;
 
         int minDist = Integer.MAX_VALUE, resNode = -1;
-        for (int i = 0; i <A.length; i++) {
+        for (int i = 0; i < A.length; i++) {
             // node is not reacheable so discard
             System.out.println(B[i]);
-            //if (A[i] == Integer.MAX_VALUE || B[i] == Integer.MAX_VALUE) continue;
+            if (A[i] == Integer.MAX_VALUE || B[i] == Integer.MAX_VALUE) continue;
             int tempDist = Math.max(A[i], B[i]);
             // take the node with min dist from node1 and node2
             if (tempDist < minDist) {
@@ -35,11 +35,11 @@ class Solution {
         return resNode;
     }
 
-    public void distace(ArrayList<ArrayList<Integer>> graph, int node1, int node2, int srs,int[]distace) {
+    public void distace(ArrayList<ArrayList<Integer>> graph, int node1, int node2, int srs, int[] distace) {
         int n = graph.size();
         boolean[] visit = new boolean[n];
         LinkedList<Integer> q = new LinkedList<>();
-        
+
         q.add(srs);
         int dist = 0;
         while (q.size() > 0) {
@@ -57,6 +57,5 @@ class Solution {
             }
             dist++;
         }
-        
     }
 }
