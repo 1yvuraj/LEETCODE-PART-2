e8182f6cee54d0829439117152cd254095add3fd
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-02 15:40:45 +0530

    Time: 3 ms (48.74%), Space: 55 MB (6.43%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,44 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+        parent=new int[n+1];
+        rank=new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+        int count=n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i != j && isConnected[i][j] == 1) {
+                    int x = findPar(i);
+                    int y = findPar(j);
+                    if (x != y) {
+                        union(x, y);
+                        count--;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public int findPar(int u) {
+        return parent[u] == u ? u : (parent[u] = findPar(parent[u]));
+    }
+
+    public void union(int s1, int s2) {
+        if (rank[s1] < rank[s2]) {
+            parent[s1] = s2;
+        } else if (rank[s1] > rank[s2]) {
+            parent[s2] = s1;
+        } else {
+            parent[s1] = s2;
+            rank[s1]++;
+        }
+    }
+}
