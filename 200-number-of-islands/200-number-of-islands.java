affb59b30bc75ce0bf8ab55ae07789120581332c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-02 19:20:19 +0530

    Time: 6 ms (40.58%), Space: 57.4 MB (54.47%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,25 +1,17 @@
 class Solution {
     int[] p;
-    int[] rank;
+    //int[] rank;
 
     public int numIslands(char[][] grid) {
         p = new int[grid.length * grid[0].length];
-        rank = new int[grid.length * grid[0].length];
+        //rank = new int[grid.length * grid[0].length];
         int max = -(int) 1e9;
         int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
         for (int i = 0; i < p.length; i++) {
             p[i] = i;
-            rank[i] = 1;
+            
         }
         int ans = 0;
-        // for (int i = 0; i < grid.length; i++) {
-        //     for (int j = 0; j < grid[0].length; j++) {
-        //         if (grid[i][j] - '0' == 1) {
-        //             ans++;
-        //         }
-        //     }
-        // }
-
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] - '0' == 1) {
@@ -33,7 +25,7 @@ class Solution {
                             if (x != y) {
                                 p[y] = x;
                                 ans--;
-                                rank[x] += rank[y];
+                                //rank[x] += rank[y];
                             }
                         }
                     }
