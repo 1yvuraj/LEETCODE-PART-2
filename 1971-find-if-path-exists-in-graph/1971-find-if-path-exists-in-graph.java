6c4af670c069cca7b050d89822ec17ce8c13434a
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-03 12:43:10 +0530

    Time: 274 ms (30.41%), Space: 220.3 MB (36.45%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,27 +1,30 @@
 class Solution {
-    int[]parent;
-    
+
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        parent=new int[n];
-        for(int i=0;i<parent.length;i++){
-            parent[i]=i;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        if (edges.length == 0) return true;
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-        for(int []nbr:edges){
-            int x=findPar(nbr[0]);
-            int y=findPar(nbr[1]);
-            if(x!=y){
-                parent[x]=y;
-               
+        for (int i = 0; i < edges.length; i++) {
+            graph.get(edges[i][0]).add(edges[i][1]);
+            graph.get(edges[i][1]).add(edges[i][0]);
+        }
+        boolean[] visit = new boolean[n];
+
+        visit = new boolean[n];
+        dfs(graph, source, visit);
+        if (visit[destination]) return true;
+
+        return false;
+    }
+
+    public void dfs( ArrayList<ArrayList<Integer>> graph, int src, boolean[] visit) {
+        visit[src] = true;
+        for (int nbr : graph.get(src)) {
+            if (!visit[nbr]) {
+                dfs(graph, nbr, visit);
             }
         }
-        
-        
-        
-        return findPar(source)==findPar(destination);
-        
-    }
-    public int findPar(int u) {
-        return parent[u] == u ? u : (parent[u] = findPar(parent[u]));
     }
 }
-
