25a5601a04d7c5cb5828d2295a204f91a10a27d8
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-03 12:27:40 +0530

    Time: 14 ms (96.89%), Space: 138.1 MB (85.55%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,35 +1,27 @@
 class Solution {
-
+    int[]parent;
+    
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        if (edges.length == 0) return true;
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+        parent=new int[n];
+        for(int i=0;i<parent.length;i++){
+            parent[i]=i;
         }
-        for (int i = 0; i < edges.length; i++) {
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
-        }
-        LinkedList<Integer> q = new LinkedList<>();
-        boolean[] visit = new boolean[n];
-        q.addLast(source);
-        while (q.size() > 0) {
-            int size = q.size();
-            while (size-- > 0) {
-                int rem = q.removeFirst();
-                if (visit[rem]) continue;
-                visit[rem] = true;
-                if (rem == destination) {
-                    System.out.println(rem);
-                    return true;
-                }
-                for (int nbr : graph.get(rem)) {
-                    if (!visit[nbr]) {
-                        q.addLast(nbr);
-                    }
-                }
+        for(int []nbr:edges){
+            int x=findPar(nbr[0]);
+            int y=findPar(nbr[1]);
+            if(x!=y){
+                parent[x]=y;
+               
             }
         }
-        return false;
+        
+        
+        
+        return findPar(source)==findPar(destination);
+        
+    }
+    public int findPar(int u) {
+        return parent[u] == u ? u : (parent[u] = findPar(parent[u]));
     }
 }
+
