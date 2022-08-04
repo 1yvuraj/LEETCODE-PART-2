05add8922274a343f72565e1d2836e37217cb83a
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-04 16:57:12 +0530

    Time: 6 ms (85.27%), Space: 63.7 MB (81.63%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,8 +1,7 @@
 class Solution {
     int[] parent;
-
     public int makeConnected(int n, int[][] connections) {
-        if(connections.length<n-1)return -1;
+        if (connections.length < n - 1) return -1;
         parent = new int[n];
         for (int i = 0; i < parent.length; i++) {
             parent[i] = i;
@@ -12,16 +11,16 @@ class Solution {
             int x = findPar(nbr[0]);
             int y = findPar(nbr[1]);
             if (x != y) {
-                parent[y] = x;
+                parent[x] = y;
             }
         }
 
         for (int i = 0; i < parent.length; i++) {
-        if (parent[i]== i) {
+            if (parent[i] == i) {
                 c++;
             }
         }
-        return c-1;
+        return c - 1;
     }
 
     public int findPar(int u) {
