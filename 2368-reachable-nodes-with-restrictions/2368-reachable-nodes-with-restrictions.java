0e3cce374c8e998ebc98f4e895f650041b8a7bc6
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-07 22:11:22 +0530

    Time: 96 ms (42.86%), Space: 180.9 MB (14.29%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
@@ -1,33 +1,32 @@
 class Solution {
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
-        ArrayList<Integer>[]graph=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            graph[i]=new ArrayList<>();
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        for(int[]nbr:edges){
+        for (int[] nbr : edges) {
             graph[nbr[0]].add(nbr[1]);
-             graph[nbr[1]].add(nbr[0]);
-            
+            graph[nbr[1]].add(nbr[0]);
         }
-        LinkedList<Integer>q=new LinkedList<>();
+        LinkedList<Integer> q = new LinkedList<>();
         q.addLast(0);
-        HashSet<Integer>set=new HashSet<>();
-        for(int i=0;i<restricted.length;i++){
-            set.add(restricted[i]);
+        boolean[] visit = new boolean[n];
+        for (int i = 0; i < restricted.length; i++) {
+           visit[restricted[i]]=true;
         }
-        boolean[]visit=new boolean[n];
-        int count=0;
-        while(q.size()>0){
-            int rem=q.removeFirst();
-            if(visit[rem])continue;
-            if(set.contains(rem))continue;
-            visit[rem]=true;
+        
+       
+        int count = 0;
+        while (q.size() > 0) {
+            int rem = q.removeFirst();
+            if (visit[rem]) continue;
+           
+            visit[rem] = true;
             ++count;
-            for(int nbr:graph[rem]){
-                if(!visit[nbr]){
+            for (int nbr : graph[rem]) {
+                if (!visit[nbr]) {
                     // System.out.println(nbr);
                     q.addLast(nbr);
-                    
                 }
             }
         }
