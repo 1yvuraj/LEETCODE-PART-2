d4d29818b0eaca316b934d80a6b20891be3b81bb
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-09 15:06:17 +0530

    Time: 29 ms (9.09%), Space: 98.7 MB (9.09%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validPartition(int[] nums) {
+        Boolean[]dp=new Boolean[nums.length];
+        return help(nums,0,dp);
+    }
+    public boolean help(int[] nums,int i,Boolean []dp) {
+       if(i==nums.length)return true;
+        if(dp[i]!=null)return dp[i];
+       if(i+1<nums.length && nums[i]==nums[i+1]){
+           if(help(nums,i+2,dp))return dp[i]=true;
+            if(i+2<nums.length && nums[i+2]==nums[i]){
+            if(help(nums,i+3,dp))return dp[i]=true;
+           }
+        }
+        if(i+2<nums.length && nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]){
+           
+            if(help(nums,i+3,dp))return dp[i]=true;
+           
+        }
+         return dp[i]=false;
+    }
+}
