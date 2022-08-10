b63dd33ade5aef1ca6cf450267222ae97350c258
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-10 12:00:59 +0530

    Time: 504 ms (12.55%), Space: 211.1 MB (25.85%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,25 +1,61 @@
 class Solution {
-    int max = -1;
+    int ans = 0;
     public int longestCycle(int[] edges) {
-        max = -(int) 1e9;
-        boolean[] visit = new boolean[edges.length];
-       for(int i=0 ;i<edges.length;i++){
-            HashMap<Integer, Integer> map = new HashMap<>();
-            if (edges[i]==-1 ||visit[i]) continue;
-            dfs(edges, i, 0, map, visit);
+        LinkedList<Integer> stack = new LinkedList<>();
+        int n = edges.length;
+        int max = -(int) 1e9;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < edges.length; i++) {
+            graph.add(new ArrayList<>());
         }
-        return max==-(int)1e9?-1:max;
+        for (int i = 0; i < n; i++) {
+            if(edges[i]!=-1)
+            graph.get(i).add(edges[i]);
+        }
+        boolean[] visit = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(i, graph, visit, stack);
+            }
+        }
+        ArrayList<ArrayList<Integer>> ngraph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ngraph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            if(edges[i]!=-1)
+            ngraph.get(edges[i]).add(i);
+        }
+        visit = new boolean[n];
+       
+        while (stack.size() > 0) {
+            int rem = stack.removeFirst();
+            if (!visit[rem]) {
+                ans = 0;
+                dfs1(rem, ngraph, visit);
+                max = Math.max(max, ans);
+            }
+        }
+        return max==1?-1:max;
     }
 
-    public void dfs(int[] edges, int src, int dist, HashMap<Integer,Integer> map, boolean[] visit) {
-        if (src == -1) return;
-        if (visit[src]) {
-            if (!map.containsKey(src)) return;
-            max = Math.max(max, dist - map.get(src));
-            return;
-        }
+    public void dfs(int src, ArrayList<ArrayList<Integer>> graph, boolean[] visit, LinkedList<Integer> stack) {
         visit[src] = true;
-        map.put(src, dist);
-        dfs(edges, edges[src], dist + 1, map, visit);
+        for (int v : graph.get(src)) {
+            if (!visit[v]) {
+                dfs(v, graph, visit, stack);
+            }
+        }
+        stack.addFirst(src);
+    }
+
+    public void dfs1(int src, ArrayList<ArrayList<Integer>> graph, boolean[] visit) {
+        visit[src] = true;
+        ans++;
+        for (int v : graph.get(src)) {
+            if (!visit[v]) {
+                dfs1(v, graph, visit);
+            }
+        }
     }
 }
