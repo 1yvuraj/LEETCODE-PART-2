067dc6ddf00b33bf996d0f7f00f25f36d6a284ac
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-22 10:55:51 +0530

    Time: 470 ms (20.00%), Space: 42.6 MB (50.00%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,10 +1,36 @@
 class Solution {
+
+    // public int secondsToRemoveOccurrences(String s) {
+    //     int time=0;
+    //     while(s.indexOf("01")>=0){
+    //         s=s.replace("01","10");
+    //         time++;
+    //     }
+    //     return time;
+    // }
     public int secondsToRemoveOccurrences(String s) {
-        int time=0;
-        while(s.indexOf("01")>=0){
-            s=s.replace("01","10");
+        StringBuilder sb = new StringBuilder(s);
+        int count = 0;
+        int time = 0;
+        while (count >=0) {
+            count = 0;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if (sb.charAt(i) == '0' && sb.charAt(i+1) == '1') {
+                    count++;
+                }
+            }
+            if (count == 0) return time;
+            for (int i = 0; i < sb.length() - 1; i++) {
+                if (sb.charAt(i) == '0' && sb.charAt(i+1) == '1') {
+                    sb.setCharAt(i, '1');
+                    sb.setCharAt(i + 1, '0');
+                    i++;
+                }
+            }
+           
+           
             time++;
         }
-        return time;
+        return -1;
     }
 }
