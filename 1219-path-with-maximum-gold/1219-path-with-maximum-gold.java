99d69ae35108ffccba070629a50739cd7b6b0b62
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-24 22:14:47 +0530

    Time: 162 ms (23.84%), Space: 42.4 MB (20.78%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -4,28 +4,29 @@ class Solution {
         int ans = -(int)1e9;
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis= new boolean[n][m];;
+        //boolean[][] vis= new boolean[n][m];;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] != 0) {
-                    ans = Math.max(ans, help_rec(grid, i, j, n - 1, m - 1,vis));
+                    ans = Math.max(ans, help_rec(grid, i, j, n - 1, m - 1));
                 }
             }
         }
         return ans==-(int)1e9?0:ans;
     }
-    public int help_rec(int[][] arr, int sr, int sc, int dr, int dc,boolean[][]visit) {
+    public int help_rec(int[][] arr, int sr, int sc, int dr, int dc) {
         
-        int maxGold = 0;
-        visit[sr][sc]=true;
+        int maxGold =0;
+        int temp=arr[sr][sc];
+        arr[sr][sc]=0;
         for(int[] d : dir) {
             int r = sr + d[0]; 
             int c = sc + d[1];
-            if(r >=0 && c >= 0 && r <=dr && c <= dc  && arr[r][c] != 0 && visit[r][c]==false)                             {
-                maxGold = Math.max(maxGold,help_rec(arr, r, c, dr, dc,visit));
+            if(r >=0 && c >= 0 && r <=dr && c <= dc  && arr[r][c] != 0)                             {
+                maxGold = Math.max(maxGold,help_rec(arr, r, c, dr, dc));
             }
         }
-         visit[sr][sc]=false;
+         arr[sr][sc]=temp;
         
         return maxGold+arr[sr][sc];
         
