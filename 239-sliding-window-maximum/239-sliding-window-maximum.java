fd0525846d240878287cbd47bf8f12d11ad68537
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-25 21:01:33 +0530

    Time: 70 ms (37.63%), Space: 149 MB (60.07%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,26 +1,44 @@
-class Solution { 
-    private class pair implements Comparable<pair>{
-        int value;
-        int idx;
-        pair(int value,int idx){
-            this.value=value;
-            this.idx=idx;
-        }
-        public int compareTo(pair o){
-            return o.value-this.value;
-        }
-    }
+class Solution {
+
+    //     private class pair implements Comparable<pair> {
+    //         int value;
+    //         int idx;
+    //         pair(int value, int idx) {
+    //             this.value = value;
+    //             this.idx = idx;
+    //         }
+
+    //         public int compareTo(pair o) {
+    //             return o.value - this.value;
+    //         }
+    //     }
+    //     public int[] maxSlidingWindow(int[] arr, int k) {
+    //         PriorityQueue<pair> pq = new PriorityQueue<>();
+    //         int y = 0;
+    //         int[] ans = new int[arr.length - k + 1];
+    //         for (int i = 0; i < arr.length; i++) {
+    //             while (pq.size() > 0 && pq.peek().idx <= i - k) {
+    //                 pq.remove();
+    //             }
+    //             pq.add(new pair(arr[i], i));
+    //             if (i >= k - 1) ans[y++] = pq.peek().value;
+    //         }
+    //         return ans;
+    //     }
+
     public int[] maxSlidingWindow(int[] arr, int k) {
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        int y=0;
-        int[] ans = new int[arr.length- k+1];
-        for(int i=0;i<arr.length;i++){
-            while(pq.size()>0 && pq.peek().idx<=i-k){
-                pq.remove();
+        LinkedList<Integer> dq = new LinkedList<>();
+        int y = 0;
+        int[] ans = new int[arr.length - k + 1];
+        for (int i = 0; i < arr.length; i++) {
+            while (dq.size() > 0 && dq.getFirst() <= i - k) {
+                dq.removeFirst();
             }
-            pq.add(new pair(arr[i],i));
-            if(i>=k-1)ans[y++]=pq.peek().value;
-            
+            while (dq.size() > 0 && arr[dq.getLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if (i >= k - 1) ans[y++] = arr[dq.getFirst()];
         }
         return ans;
     }
