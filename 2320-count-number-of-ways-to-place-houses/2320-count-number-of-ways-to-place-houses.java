6d2c6e86c73a3f67b3072f7811362455d925fd62
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-26 21:29:12 +0530

    Time: 38 ms (26.53%), Space: 40.9 MB (82.39%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -1,17 +1,14 @@
 class Solution {
-
     public int countHousePlacements(int n) {
-        long[]dp1=new long[n+1];
-        long[]dp2=new long[n+1];
-        dp1[1]=1;
-        dp2[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp1[i]=dp2[i-1]%1000000007;
-            dp2[i]=dp1[i-1]%1000000007+dp2[i-1]%1000000007;
-            
-        }
-        long sum=((dp1[n]+dp2[n])*(dp1[n]+dp2[n]))%1000000007;
-        return (int)sum;
+        long zeroes = 1;
+      long ones = 1;
+      for (int i = 2; i <= n; i++) {
+         long nzeroes = ones %((int)1e9+7);
+         long nones = (ones + zeroes)%((int)1e9+7);
+
+         zeroes = nzeroes%((int)1e9+7);
+         ones = nones%((int)1e9+7);
+      }
+      return (int)(((zeroes+ones)*(zeroes+ones))%((int)1e9+7));
     }
 }
