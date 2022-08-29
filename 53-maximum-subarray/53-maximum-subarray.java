afb9c06925bd410532e0cf4c678469c1e86f9d04
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-08-29 23:05:07 +0530

    Time: 2 ms (74.59%), Space: 75.7 MB (21.47%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,17 +1,20 @@
 class Solution {
 
     public int maxSubArray(int[] nums) {
-       int gsum=0,cursum=0;
-        for(int i=0;i<nums.length;i++){
-            cursum+=nums[i];
-            if(cursum>gsum)gsum=cursum;
-            if(cursum<=0){
-                cursum=0;
+        int gsum = -(int)1e9, cursum = 0, gs = 0, ge = 0, cs = 0;
+        for (int i = 0; i < nums.length; i++) {
+            cursum += nums[i];
+            if (cursum > gsum) {
+                gsum = cursum;
+                gs = cs;
+                ge = i;
+            }
+            if (cursum <= 0) {
+                cursum = 0;
+                cs = i + 1;
             }
         }
-        if(gsum==0){
-            Arrays.sort(nums);
-        }
-        return gsum==0?nums[nums.length-1]:gsum;
+       
+        return gsum ;
     }
 }
