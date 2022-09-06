c5c1956404bc8d324d457e5f44edb856cbd88904
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-06 11:55:23 +0530

    Time: 1 ms (73.24%), Space: 40.6 MB (75.59%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.java
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -1,51 +1,44 @@
 class Solution {
+
     public int nextGreaterElement(int n) {
         long m=n;
-        char[] ch=(""+m).toCharArray();
-        int idx=-1;
-        int j=-1;
-         for(int i=ch.length-1;i>0;i--){
-             if(ch[i-1]<ch[i]){
-                 idx=i-1;
-                 break;
-             }
-         }
-        if(idx==-1){
-            return -1;
-        }
-        if(idx>-1){
-            for(j=ch.length-1;j>idx;j--){
-            if(ch[j]>ch[idx]){
-                swap(ch,idx,j);
+        char[] number =(""+m).toCharArray();
+        int idx = -1;
+        for (int i = number.length - 1; i > 0; i--) {
+            if (number[i - 1] < number[i]) {
+                idx = i - 1;
                 break;
             }
         }
-            reverse(ch,idx+1,ch.length-1);
+        if (idx == -1) return -1;
+
+        for (int i = number.length - 1; i > idx; i--) {
+            if (number[i] > number[idx]) {
+                swap(number, i, idx);
+                break;
+            }
         }
-        
-        
-        String str=new String(ch);
-        long ans=Long.parseLong(str);
-        if(ans<=Integer.MAX_VALUE){
-            return (int)ans;
-        }
-        
-        else{
+        reverse(number, idx + 1, number.length - 1);
+
+        String str = new String(number);
+        if (Long.parseLong(str) <= Integer.MAX_VALUE) {
+            return Integer.parseInt(str);
+        } else {
             return -1;
         }
     }
-    public static void swap(char[] ch,int i,int j){
-        char temp=ch[i];
-        ch[i]=ch[j];
-        ch[j]=temp;
+
+    public static void swap(char[] ch, int i, int j) {
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
     }
-    
-    public static void reverse(char[] ch,int i,int j){
-        while(i<j){
-            swap(ch,i,j);
+
+    public static void reverse(char[] ch, int i, int j) {
+        while (i < j) {
+            swap(ch, i, j);
             i++;
             j--;
         }
-
     }
 }
