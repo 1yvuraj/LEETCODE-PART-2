383076f08bdc0ed2ec86f241e0bace0264ed1783
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-12 20:24:28 +0530

    Time: 1 ms (53.62%), Space: 40.8 MB (67.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,43 +1,15 @@
 class Solution {
-
     public int uniquePaths(int m, int n) {
-        int[][] dir = { { 1, 0 }, { 0, 1 } };
-        int[][]dp=new int[m][n];
-        return help(0, 0, m - 1, n - 1, dir,dp);
+        int [][]dp=new int[m+1][n+1];
+        return help(0,0,m,n,dp);
     }
-
-    // public int help(int sr, int sc, int er, int ec, int[][] dir) {
-    //     if (sr == er && sc == ec) return 1;
-    //     int count = 0;
-    //     for (int d = 0; d < dir.length; d++) {
-    //         int r = sr + dir[d][0];
-    //         int c = sc + dir[d][1];
-    //         if (r >= 0 && c >= 0 && r <= er && c <= ec) {
-    //             count += help(r, c, er, ec, dir);
-    //         }
-    //     }
-    //     return count;
-    // }
-
-    public int help(int sr, int sc, int er, int ec, int[][] dir,int[][]dp) {
-        for (int i = er; i >= 0; i--) {
-            for (int j = ec; j >= 0; j--) {
-                if (i == er && j == ec) 
-                {
-                    dp[i][j]= 1;
-                    continue;
-                }
-                int count = 0;
-                for (int d = 0; d < dir.length; d++) {
-                    int r = i + dir[d][0];
-                    int c = j + dir[d][1];
-                    if (r >= 0 && c >= 0 && r <= er && c <= ec) {
-                        count += dp[r][c];
-                    }
-                }
-                dp[i][j]=count;
-            }
-        }
-        return dp[0][0];
+    public int help(int sr, int sc,int er,int ec,int[][]dp) {
+        if(sr==er-1 && sc==ec-1) return 1;
+        if(sr>er || sc>ec)return 0;
+        if(dp[sr][sc]!=0)return dp[sr][sc] ;
+        int down=help(sr+1,sc,er,ec,dp);
+        int right=help(sr,sc+1,er,ec,dp);
+        return dp[sr][sc]=down+right;
+        
     }
 }
