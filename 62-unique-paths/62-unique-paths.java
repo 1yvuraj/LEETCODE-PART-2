8b318601a1e679b2a917fd910541069f3a432493
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-12 20:26:18 +0530

    Time: 1 ms (53.62%), Space: 41.3 MB (28.32%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,10 +1,10 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int [][]dp=new int[m+1][n+1];
-        return help(0,0,m,n,dp);
+        int [][]dp=new int[m][n];
+        return help(0,0,m-1,n-1,dp);
     }
     public int help(int sr, int sc,int er,int ec,int[][]dp) {
-        if(sr==er-1 && sc==ec-1) return 1;
+        if(sr==er && sc==ec) return 1;
         if(sr>er || sc>ec)return 0;
         if(dp[sr][sc]!=0)return dp[sr][sc] ;
         int down=help(sr+1,sc,er,ec,dp);
