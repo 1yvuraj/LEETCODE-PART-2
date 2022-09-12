80faa6e1956648c6b1a764ba04e743c94cb1371b
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-12 21:00:45 +0530

    Time: 1 ms (53.62%), Space: 41.2 MB (35.03%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,15 +1,22 @@
 class Solution {
+    int[][]dir={{1,0},{0,1}};
     public int uniquePaths(int m, int n) {
-        int [][]dp=new int[m+1][n+1];
-        return help(0,0,m-1,n-1,dp);
+        int[][] dp = new int[m + 1][n + 1];
+        return help( 0,0,m - 1, n - 1, dp);
     }
-    public int help(int sr, int sc,int er,int ec,int[][]dp) {
-        if(sr==er && sc==ec) return 1;
-        if(sr>er || sc>ec)return 0;
-        if(dp[sr][sc]!=0)return dp[sr][sc] ;
-        int down=help(sr+1,sc,er,ec,dp);
-        int right=help(sr,sc+1,er,ec,dp);
-        return dp[sr][sc]=down+right;
-        
+    public int help( int sr,int sc,int er, int ec, int[][] dp) {
+        if(sr==er && sc==ec)return 1;
+        if(dp[sr][sc]!=0)return dp[sr][sc];
+        int count=0;
+        for(int []d:dir){
+            int r=sr+d[0];
+            int c=sc+d[1];
+            if(r>=0 && c>=0 && r<=er && c<=ec){
+                count+=help(r,c,er,ec,dp);
+            }
+        }
+       return dp[sr][sc]=count;
+    
     }
+     
 }
