a4f3258c0ad2ef1ebaf159b403e794a471cff138
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-13 20:51:40 +0530

    Time: 2 ms (68.01%), Space: 42.2 MB (61.80%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,23 +1,33 @@
 class Solution {
+
     public int numDecodings(String s) {
-        int[]dp=new int[s.length()+1];
-        Arrays.fill(dp,-1);
-        return help(s,0,dp);
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+        return help(s, 0, dp);
     }
-    public int help(String s,int idx,int[]dp) {
-        if(idx==s.length())return dp[idx]=1;
-        if(dp[idx]!=-1)return dp[idx];
-        char ch=s.charAt(idx);
-        int count=0;
-        if(ch=='0')return 0;
-        count+=help(s,idx+1,dp);
-        if(idx<s.length()-1){
-            char ch1=s.charAt(idx+1);
-            int num=(ch-'0')*10+(ch1-'0');
-            if(num<=26){
-                count+=help(s,idx+2,dp);
+
+    public int help(String s, int IDX, int[] dp) {
+        for (int idx = s.length(); idx >= 0; idx--) {
+            if (idx == s.length()) {
+                dp[idx] = 1;
+                continue;
             }
+            char ch = s.charAt(idx);
+            int count = 0;
+            if (ch == '0'){
+                dp[idx]= 0;
+                continue;
+            } 
+            count += dp[idx + 1];
+            if (idx < s.length() - 1) {
+                char ch1 = s.charAt(idx + 1);
+                int num = (ch - '0') * 10 + (ch1 - '0');
+                if (num <= 26) {
+                    count += dp[idx + 2];
+                }
+            }
+            dp[idx] = count;
         }
-        return dp[idx]=count;
+        return dp[IDX];
     }
 }
