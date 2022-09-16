423a06a03231922f68724e741f4ca9904232bda6
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-16 19:53:30 +0530

    Time: 31 ms (49.03%), Space: 49.9 MB (65.94%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -6,17 +6,39 @@ class Solution {
         return longestPlaindromicSubsequence_memo(text1, text2, text1.length(), text2.length(), dp);
     }
 
-    public static int longestPlaindromicSubsequence_memo(String s1, String s2, int n, int m, int[][] dp) {
-        if (m == 0) {
-            return dp[n][m] = 1;
+    //     public static int longestPlaindromicSubsequence_memo(String s1, String s2, int n, int m, int[][] dp) {
+    //         if (m == 0) {
+    //             return dp[n][m] = 1;
+    //         }
+    //         if (n < m) return dp[n][m] = 0;
+    //         if (dp[n][m] != -1) return dp[n][m];
+
+    //         if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+    //             return dp[n][m] = longestPlaindromicSubsequence_memo(s1, s2, n - 1, m - 1, dp) + longestPlaindromicSubsequence_memo(s1, s2, n - 1, m, dp);
+    //         } else {
+    //             return dp[n][m] = longestPlaindromicSubsequence_memo(s1, s2, n - 1, m, dp);
+    //         }
+    //     }
+
+    public static int longestPlaindromicSubsequence_memo(String s1, String s2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (m == 0) {
+                    dp[n][m] = 1;
+                    continue;
+                }
+
+                if (n < m) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+                if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+                    dp[n][m] = dp[n - 1][m - 1] + dp[n - 1][m];
+                } else {
+                    dp[n][m] = dp[n - 1][m];
+                }
+            }
         }
-        if (n < m) return dp[n][m] = 0;
-        if (dp[n][m] != -1) return dp[n][m];
-        
-        if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
-            return dp[n][m] = longestPlaindromicSubsequence_memo(s1,s2,n-1,m-1,dp)+longestPlaindromicSubsequence_memo(s1,s2,n-1,m,dp);
-        } else {
-            return dp[n][m] = longestPlaindromicSubsequence_memo(s1,s2,n-1,m,dp);
-        }
+        return dp[N][M];
     }
 }
