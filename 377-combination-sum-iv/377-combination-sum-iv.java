f5a06e6df778b7578e5987fa134e6938d5286c32
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-29 23:57:13 +0530

    Time: 29 ms (5.43%), Space: 45.1 MB (5.40%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,17 +1,23 @@
 class Solution {
-    public int combinationSum4(int[] nums, int target) {
-        int[]dp=new int [target+1];
-        dp[0]=1;
-        for(int i=1;i<dp.length;i++)
-        {
-            for(int j=0;j<nums.length;j++)
-            {
-                if(i-nums[j]>=0)
-                {
-                    dp[i]+=dp[i-nums[j]];
-                }
+
+    public int combinationSum4(int[] coins, int target) {
+        int[] dp = new int[target+1];
+        Arrays.fill(dp,-1);
+        return coinChangeCombination_IN(coins, target, "", dp);
+    }
+
+    public static int coinChangeCombination_IN(int[] coins, int tar, String psf, int[] dp) {
+        if (tar == 0) {
+            System.out.println(psf);
+            return 1;
+        }
+        if (dp[tar] != -1) return dp[tar];
+        int count = 0;
+        for (int i = 0; i < coins.length; i++) {
+            if (tar - coins[i] >= 0) {
+                count += coinChangeCombination_IN(coins, tar - coins[i], psf + coins[i] + " ", dp);
             }
         }
-        return dp[dp.length-1];
+        return dp[tar] = count;
     }
 }
