e61df456800c190a8c543576f1083d8021d70893
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-09-30 23:40:48 +0530

    Time: 2206 ms (5.01%), Space: 304.9 MB (5.15%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coinChange(int[] coins, int target) {
+        int[]dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        return coinChangeCombination_IN(coins, target, "", dp)==1000000001?-1: coinChangeCombination_IN(coins, target, "", dp);
+    }
+    public static int coinChangeCombination_IN(int[] coins, int tar, String psf,int[]dp) {
+        if (tar == 0) {
+            System.out.println(psf);
+            return 0;
+        }
+        if(dp[tar]!=-1)return dp[tar];
+        int count = (int)1e9;
+        for (int i = 0; i < coins.length; i++) {
+            if (tar - coins[i] >= 0) {
+                count = Math.min(count,coinChangeCombination_IN(coins, tar - coins[i], psf + coins[i] + " ",dp));
+            }
+        }
+        return dp[tar]=count+1;
+    }
+}
