748545e0738f7d5eabeb7cccc1411eec0382831b
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-10-06 22:22:05 +0530

    Time: 1209 ms (33.67%), Space: 41.6 MB (74.34%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -6,23 +6,22 @@ class Solution {
             sum += val;
             max = Math.max(max, val);
         }
-        boolean[] vis = new boolean[nums.length];
         if (sum % k == 1 || max > sum / k) return false;
-        return check(nums, 0, 0, sum / k, k, vis);
+        return check(nums, 0, 0, sum / k, k);
     }
 
-    public boolean check(int[] nums, int idx, int sumsf, int tar, int k, boolean[] v) {
+    public boolean check(int[] nums, int idx, int sumsf, int tar, int k) {
         if (k == 0) return true;
         if (sumsf > tar) return false;
         if (tar == sumsf) {
-            return check(nums, 0, 0, tar, k - 1, v);
+            return check(nums, 0, 0, tar, k - 1);
         }
         boolean res = false;
         for (int i = idx; i < nums.length; i++) {
             if (tar - nums[i] >= 0 && nums[i]>-1) {
                 int val = nums[i];
                 nums[i]=-1;
-                res = res || check(nums, i + 1, sumsf + val, tar, k, v);
+                res = res || check(nums, i + 1, sumsf + val, tar, k);
                 nums[i]=val;
             }
         }
