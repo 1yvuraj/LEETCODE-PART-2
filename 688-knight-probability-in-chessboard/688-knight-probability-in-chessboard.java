d01ed3f060d38de43d59ade2afaa2141e01af334
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-10-07 22:28:14 +0530

    Time: 5 ms (98.44%), Space: 41.2 MB (98.44%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.java
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.java
@@ -1,21 +1,22 @@
 class Solution {
-     int[][] dir = {{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2},{1,-2},{2,-1}};
+    int[][] dir = { { 2, 1 }, { 1, 2 }, { -1, 2 }, { -2, 1 }, { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 } };
+
     public double knightProbability(int n, int k, int row, int column) {
-        double[][][]dp=new double[k+1][n+1][n+1];
-        return knightProbability_(n, k, row, column,dp);
+        double[][][] dp = new double[k + 1][n + 1][n + 1];
+        return knightProbability_(n, k, row, column, dp);
     }
 
-    public double knightProbability_(int n, int k, int row, int column,double [][][]dp) {
+    public double knightProbability_(int n, int k, int row, int column, double[][][] dp) {
         if (k == 0) return 1.0;
-        if(dp[k][row][column]!=0.0)return dp[k][row][column];
+        if (dp[k][row][column] != 0.0) return dp[k][row][column];
         double count = 0.0;
         for (int d = 0; d < 8; d++) {
             int r = row + dir[d][0];
             int c = column + dir[d][1];
             if (r >= 0 && c >= 0 && r < n && c < n) {
-                count += knightProbability_(n, k - 1, r, c,dp);
+                count += knightProbability_(n, k - 1, r, c, dp);
             }
         }
-        return dp[k][row][column]=count / 8.0;
+        return dp[k][row][column] = count*1.0/8.0 ;
     }
 }
