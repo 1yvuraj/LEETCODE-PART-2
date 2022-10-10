c091011033f76615673876bdcd4615a4f7965899
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-10-11 00:26:06 +0530

    Time: 35 ms (27.05%), Space: 43.4 MB (78.83%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -1,70 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    long ans = 0;
-
+    int ans=0;
     public int pathSum(TreeNode root, int targetSum) {
-        ans = 0;
-        help(root, targetSum);
-        return (int)ans;
+        ans=0;
+        help1(root,targetSum);
+        return ans;
     }
-
-    public void help1(TreeNode root,long sum, int targetSum) {
-        if (root == null) return;
-        if (targetSum == sum+root.val) ans++;
-        help1(root.left,sum+root.val, targetSum );
-        help1(root.right,sum+root.val, targetSum );
+    public void help2(TreeNode root, long sum,int targetSum) {
+        if(root==null)return;
+        if(sum+root.val==targetSum)ans++;
+        help2(root.left,sum+root.val,targetSum);
+        help2(root.right,sum+root.val,targetSum);
+        
     }
-
-    public void help(TreeNode root, int targetSum) {
-        if (root == null) return;
-        help1(root,0, targetSum);
-        help(root.left, targetSum);
-        help(root.right, targetSum);
+     public void help1(TreeNode root,int targetSum) {
+        if(root==null)return;
+        help2(root,0,targetSum);
+        help1(root.left,targetSum);
+        help1(root.right,targetSum);
+        
     }
 }
-// class Solution {
-// int count = 0;
-// public int pathSum(TreeNode root, int targetSum) {
-
-//     countTarget_temp(root , targetSum);
-//     return count;
-// }
-// public void countTarget_temp(TreeNode node , int targetSum){
-//     if(node == null){
-//         return;
-//     }
-//     //if(node.val == -2)
-//     countTarget(node , 0 , targetSum);
-    
-//     countTarget_temp(node.left , targetSum);
-//     countTarget_temp(node.right , targetSum);
-// }
-
-// public void countTarget(TreeNode node , long csum , int targetSum){
-//     if(node == null){
-
-//         return;
-//     }
-//     //System.out.println(node.val + " " + csum);
-//     if(csum+node.val == targetSum){
-//         //System.out.println(csum  + " " + node.val);
-//         count++;
-//     }
-//     countTarget(node.left , csum+node.val , targetSum);
-//     countTarget(node.right , csum+node.val , targetSum);
-// }
-// }
