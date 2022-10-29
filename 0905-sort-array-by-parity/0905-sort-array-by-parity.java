51290a92727cab75fabc1bce027b79cc52864f15
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-10-29 14:25:08 +0530

    Time: 2 ms (60.79%), Space: 48.5 MB (27.28%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int si=0,ei=0;
+        while(ei<nums.length){
+            while(ei<nums.length && nums[ei]%2!=0){
+                ei++;
+            }
+            swap(si,ei,nums);
+            si++;
+            ei++;
+        }
+        return nums;
+        
+    }
+    public static void swap(int si,int ei,int[] nums) {
+        if(ei<nums.length){
+        int temp=nums[si];
+        nums[si]=nums[ei];
+        nums[ei]=temp;
+        }
+    }
+}
