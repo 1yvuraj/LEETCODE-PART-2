dd55f35d2cf41d79a64abc4ebac6f6b539e991a7
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-11-04 20:46:53 +0530

    Time: 23 ms (42.04%), Space: 58.2 MB (5.97%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,26 +1,20 @@
 class Solution {
-   public boolean isPossible(int[] target) {
-	if (target.length == 1) return target[0] == 1;
+    public boolean isPossible(int[] target) {
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long sum = 0;
+        for (int i = 0; i < target.length; i++) {
+            pq.add((long) target[i]);
+            sum += target[i];
+        }
+        while (pq.peek() != 1) {
+            long max = pq.poll();
+            sum = sum - max;
+            if (sum <= 0 || sum >= max) return false;
+            max = max % sum;
 
-	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-	int sum = 0;
-	for (int t: target) {
-		pq.add(t);
-		sum += t;
-	}
-
-	while (pq.peek() != 1) {
-		int curr = pq.poll();
-		if (sum - curr == 1) return true;
-
-		int x = curr % (sum - curr);
-		sum = sum - curr + x;
-
-		if (x == 0 || x == curr) return false;
-		else pq.add(x);
-	}
-
-	return true;
-}
-
+            sum += max;
+            pq.add(max > 0 ? max : sum);
+        }
+        return true;
+    }
 }
