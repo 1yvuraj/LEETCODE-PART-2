cea6c8a3cc24bf1ce817c16e9f463fbf0a09d632
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-11-05 12:04:25 +0530

    Time: 6 ms (84.11%), Space: 43.7 MB (81.31%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.java
+++ 0899-orderly-queue/0899-orderly-queue.java
@@ -1,44 +1,45 @@
-// class Solution {
-//     public String orderlyQueue(String s, int k) {
-//         String ans = s;
-//         if (k == 1) {
-//             StringBuilder sb = new StringBuilder(s);
-//             int n = s.length();
-//             while (n-- > 0) {
-//                 char ch = sb.charAt(0);
-//                 sb.deleteCharAt(0);
-//                 sb.append(ch);
-//                 String a = sb.toString();
-//                 if (ans.compareTo(a) > 0) {
-//                     ans = a;
-//                 }
-//             }
-//             return ans;
-//         }
-//         char[] arr = s.toCharArray();
-//         Arrays.sort(arr);
-//         return arr.toString();
-//     }
-// }
 class Solution {
     public String orderlyQueue(String s, int k) {
-        if(k>1){ //for k>1, the result string will always be sorted, it works like adjacent swapping in insertion sort
-            char []ch=s.toCharArray();
-            Arrays.sort(ch);
-            String snew=new String(ch);
-            return snew;
-        }
-        String ans=s;
-        StringBuilder sb=new StringBuilder(s);
-        for(int i=0;i<s.length();i++){
-            char ch=sb.charAt(0);
-            sb.deleteCharAt(0);
-            sb.append(ch);
-            String snew=sb.toString();
-            if(ans.compareTo(snew)>0){
-                ans=snew;
+        String ans = s;
+        if (k == 1) {
+            StringBuilder sb = new StringBuilder(s);
+            int n = s.length();
+            while (n-- > 0) {
+                char ch = sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(ch);
+                String a = sb.toString();
+                if (ans.compareTo(a) > 0) {
+                    ans = a;
+                }
             }
+            return ans;
         }
-        return ans;
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+         String snew=new String(arr);
+        return snew;
     }
 }
+// class Solution {
+//     public String orderlyQueue(String s, int k) {
+//         if(k>1){ //for k>1, the result string will always be sorted, it works like adjacent swapping in insertion sort
+//             char []ch=s.toCharArray();
+//             Arrays.sort(ch);
+//             String snew=new String(ch);
+//             return snew;
+//         }
+//         String ans=s;
+//         StringBuilder sb=new StringBuilder(s);
+//         for(int i=0;i<s.length();i++){
+//             char ch=sb.charAt(0);
+//             sb.deleteCharAt(0);
+//             sb.append(ch);
+//             String snew=sb.toString();
+//             if(ans.compareTo(snew)>0){
+//                 ans=snew;
+//             }
+//         }
+//         return ans;
+//     }
+// }
