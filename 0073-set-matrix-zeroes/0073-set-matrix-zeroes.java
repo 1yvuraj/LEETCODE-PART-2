f1cd1a79b83cbe36fdeaa386ad5ac5bac1c6816c
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-11-12 00:40:17 +0530

    Time: 2 ms (62.19%), Space: 54.3 MB (26.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,22 +1,48 @@
 class Solution {
 
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer>row=new HashSet<>();
-        HashSet<Integer>col=new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean row = false, col = false;
+
+        for (int i = 0; i < n; i++) if (matrix[i][0] == 0) col = true;
+        for (int j = 0; j < m; j++) if (matrix[0][j] == 0) row = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    row.add(i);
-                    col.add(j);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(row.contains(i)||col.contains(j)){
-                    matrix[i][j]=0;
+
+        for (int i = 1; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = 0;
                 }
             }
-         }
+        }
+
+        for (int j = 1; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < n; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (row) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
