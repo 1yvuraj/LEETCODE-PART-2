d6d3721a13a4db1a9b8b62cf042534e50ce40b9a
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2022-11-13 18:04:13 +0530

    Time: 43 ms (47.20%), Space: 46.2 MB (96.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,38 @@
 class Solution {
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        return kSumHelper(nums, 0, 3, 0);
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i <= arr.length - 3; i++) {
+            if (i != 0 && arr[i - 1] == arr[i]) continue;
+            int v = arr[i];
+            int target = 0 - v;
+            List<List<Integer>> two = twoSum(arr, target, i + 1, arr.length - 1);
+            for (List<Integer> val : two) {
+                val.add(v);
+                res.add(val);
+            }
+        }
+        return res;
     }
 
-    public List<List<Integer>> twoSum(int[] arr, int si, int ei, int target) {
+    public  List<List<Integer>> twoSum(int[] arr, int target, int s, int l) {
         List<List<Integer>> res = new ArrayList<>();
-        int left = si;
-        int right = ei;
-
+        int left = s;
+        int right = l;
+        Arrays.sort(arr);
         while (left < right) {
-            if (left != si && arr[left] == arr[left - 1]) {
+            if (left !=s && arr[left] == arr[left - 1]) {
                 left++;
                 continue;
             }
-
             int sum = arr[left] + arr[right];
             if (sum == target) {
-                List<Integer> list = new ArrayList<>();
-                list.add(arr[left]);
-                list.add(arr[right]);
-                res.add(list);
+                ArrayList<Integer> ans = new ArrayList<>();
 
+                ans.add(arr[left]);
+                ans.add(arr[right]);
+                res.add(ans);
                 left++;
                 right--;
             } else if (sum > target) {
@@ -32,28 +42,6 @@ class Solution {
             }
         }
 
-        return res;
-    }
-
-    public List<List<Integer>> kSumHelper(int[] nums, int target, int k, int si) {
-        if (k == 2) {
-            return twoSum(nums, si, nums.length - 1, target);
-        }
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        if (n < k) return res;
-        // Arrays.sort(nums);
-        for (int i = si; i <= n - k; i++) {
-            if (i != si && nums[i] == nums[i - 1]) continue;
-
-            int val = nums[i];
-            int targ = target - val;
-            List<List<Integer>> ans = kSumHelper(nums, targ, k - 1, i + 1);
-            for (List<Integer> list : ans) {
-                list.add(val);
-                res.add(list);
-            }
-        }
         return res;
     }
 }
