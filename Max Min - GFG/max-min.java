2e1e9d25e8a78bbe5e50023f4fe7cbecd95169ac
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2023-02-22 11:52:53 +0530

    Added solution - LeetHub

---
+++ Max Min - GFG/max-min.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+	public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.findSum(a,n));
+        }
+        
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{ 
+    public static int findSum(int nums[],int N) 
+    {
+        int max=-(int)1e9;
+        int min=(int)1e9;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+        return max+min;
+    }
+}
