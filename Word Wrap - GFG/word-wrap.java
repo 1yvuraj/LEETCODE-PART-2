e6bf86defa9e2e3137b2ed7a981c363aada5a568
YUVRAJ AGGARWAL <87929724+1yvuraj@users.noreply.github.com>
2023-08-05 23:35:14 +0530

    Added solution - LeetHub

---
+++ Word Wrap - GFG/word-wrap.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] nums = new int[n];
+            String[] S = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                nums[i]  =Integer.parseInt(S[i]);
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            System.out.println(obj.solveWordWrap(nums, k));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int solveWordWrap (int[] nums, int k)
+    {
+        int[][] dp = new int[nums.length+1][2101];
+        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i],-1);
+        return help(nums,k,0,k,dp);
+    }
+     public int help (int[] nums, int k,int i,int rem,int[][]dp)
+    {
+        if(i==nums.length)return 0;
+        if(dp[i][rem+100]!=-1)return dp[i][rem+100];
+        int ans=0;
+        if(nums[i]>rem){
+            //ye line ki matlb next line me dalna hai word 
+           ans=(rem+1)*(rem+1)+help(nums,k,i+1,k-nums[i]-1,dp);
+        }else {
+            //ye line ki matlb next line me dalna hai word 
+            int option1=0;
+            int option2=0;
+            option1=(rem+1)*(rem+1)+help(nums,k,i+1,k-nums[i]-1,dp);
+            //ye line ki matlb same  line me dalna hai word 
+            option2=help(nums,k,i+1,rem-nums[i]-1,dp);
+            ans=Math.min(option1,option2);
+        }
+        return dp[i][rem+100]=ans;
+    }
+}
